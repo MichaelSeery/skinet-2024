6d80f1a2ed0dbefcef1d3a8d42228f88d4168dce
MichaelSeery <michaelseery@outlook.com.au>
2024-08-15 10:13:25 +1000

    End of Section 16

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51Pnb8yEpPgtKuOP9DfKFYoCOuXYzcCujqYTPklO1CBhAyx3j0AIdrHOfjwxoXxjOBiXxJqk5i89rwKJPfkDWeYGX00ZjjTaMaG'
 };
